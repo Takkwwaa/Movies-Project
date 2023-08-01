a311ea42faa03acf6da4265a116890839a72a4e6
Takkwwaa <takwahosnitkd@gmail.com>
2023-08-01 11:37:37 +0100

    first commit

---
+++ Project/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-neutral-800">
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
